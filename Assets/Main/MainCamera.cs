bb10c9316f9287928d9a2da36154f5e118ec62ae
chihirobelmo <chihirobelmo@gmail.com>
2024-04-23 21:24:36 +0900

    Better underwater lighting switch...though we might need to create our own shader to calculate sun light reduction from pixel worldpos to water surface distance. also removed Main/Main folder.
    Memo:
    We can get Unity standard BRDF shaders from here.
    https://unity.com/releases/editor/archive

--- Assets/Main/MainCamera.cs
+++ Assets/Main/MainCamera.cs
@@ -36,8 +36,29 @@ public class MainCamera : MonoBehaviour {
         CameraDistanceFromPlayer_meter += Input.mouseScrollDelta.y * (CameraDistanceFromPlayer_meter / 20.0f);
         CameraDistanceFromPlayer_meter = CameraDistanceFromPlayer_meter < 65 ? 65 : CameraDistanceFromPlayer_meter > 500 ? 500 : CameraDistanceFromPlayer_meter;
 
-        theSunLight.GetComponent<Light>().intensity = transform.position.y < 0 ? 1 : 1;
-        theSunLight.GetComponent<Light>().color = transform.position.y < 0 ? new Color(0,0.2f,1.0f) : Color.white;
-        RenderSettings.skybox = transform.position.y < 0 ? theWaterBox : theSkyBox;
+        theSunLight.GetComponent<Light>().intensity = LinearSaturate(1 + transform.position.y / 300, 0, 300);
+        theSunLight.GetComponent<Light>().color = new Color(
+            /*R*/ LinearSaturate(1 + transform.position.y / 1, 0, 1),
+            /*G*/ LinearSaturate(1 + transform.position.y / 100, 0, 1),
+            /*B*/ LinearSaturate(1 + transform.position.y / 300, 1, 1)
+            );
+        RenderSettings.skybox.SetColor("_SkyTint", new Color(
+            /*R*/ LinearSaturate(1 + transform.position.y / 0.1f, 1, 1),
+            /*G*/ LinearSaturate(1 + transform.position.y / 0.1f, 1, 1),
+            /*B*/ LinearSaturate(1 + transform.position.y / 0.1f, 0, 1)
+            ));
+        RenderSettings.skybox.SetFloat("_AtmosphereThickness", LinearSaturate(0.5f - 4.5f * transform.position.y / 0.1f, 0.5f, 5));
+        RenderSettings.skybox.SetFloat("_SunSize", LinearSaturate(0.04f + 0.04f * transform.position.y / 0.1f, 0, 1));
+    }
+
+    public static float LinearSaturate(System.Func<float, float> f, float x, float minx, float maxx){
+        float ans = f(x);
+        float max = f(maxx);
+        float min = f(minx);
+        return maxx > minx ? (ans > max ? max : ans < min ? min : ans) : (ans > min ? min : ans < max ? max: ans);
+    }
+    public static float LinearSaturate(float y, float min, float max)
+    {
+        return max > min ? (y > max ? max : y < min ? min : y) : (y > min ? min : y < max ? max : y);
     }
 }

ae7b0789868fe9078f63fcddce57f69825eb0604
chihirobelmo <chihirobelmo@gmail.com>
2024-04-29 16:58:19 +0900

    MFD test

--- Assets/Avionics/MFD.cs
+++ Assets/Avionics/MFD.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 
 public class MFD : MonoBehaviour
 {
-    [SerializeField] public GameObject mfdScreen1;
+    [SerializeField] public GameObject mfdScreen;
     [SerializeField] public RenderTexture renderTexture;
     [SerializeField] public Camera mfdCamera;
     [SerializeField] public Canvas canvas;
     [SerializeField] public float mfdposoffset;
     [SerializeField] public Vector3 mfdcampos;
 
-    GameObject clientScreen1;
+    GameObject clientScreen;
     Camera clientMfdCamera;
     Canvas clientCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        clientScreen1 = Instantiate(mfdScreen1);
+        clientScreen = Instantiate(mfdScreen);
         clientMfdCamera = Instantiate(mfdCamera);
         clientCanvas = Instantiate(canvas);
 
@@ -29,6 +29,27 @@ public class MFD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateCanvasVisibility(clientCanvas, mfdcampos.x < 0 ? KeyCode.F1 : KeyCode.F2);
+        UpdateMfdPositionAndRotateCameraToLookAt(transform, clientMfdCamera, mfdcampos, mfdposoffset);
+        UpdateScreenPositionAndRotationToFolowMfdPanel(clientScreen, transform);
+        UpdateCanvasPositionOnScreen(clientCanvas, mfdcampos.x);
+    }
+
+    static void UpdateCanvasVisibility(Canvas canvas, KeyCode key)
+    {
+        canvas.enabled = Input.GetKeyUp(key) ? !canvas.enabled : canvas.enabled;
+    }
+
+    /// <summary>
+    /// Camera and MFD Panels are at the MFD Layer.
+    /// MFD Panels moves around to make sun light direction change effect.
+    /// Camera and MFD Looks at each other
+    /// Makes Panels look more realistic with specular change...
+    /// </summary>
+    /// <param name="screen"></param>
+    /// <param name="transform"></param>
+    static void UpdateMfdPositionAndRotateCameraToLookAt(Transform transform, Camera clientMfdCamera, Vector3 mfdcampos, float mfdposoffset)
+    {
         // camera positions
         clientMfdCamera.transform.position = mfdcampos;
         transform.position = clientMfdCamera.transform.position + Main.clientMainCamera.transform.forward * 1.0f;
@@ -36,20 +57,47 @@ public class MFD : MonoBehaviour
         transform.LookAt(clientMfdCamera.transform.position);
         transform.rotation *= Quaternion.Euler(90, 0, 0);
         transform.position += transform.right * mfdposoffset;
+    }
 
+    /// <summary>
+    /// Screen follows the MFD Panel
+    /// </summary>
+    /// <param name="screen"></param>
+    /// <param name="transform"></param>
+    static void UpdateScreenPositionAndRotationToFolowMfdPanel(GameObject screen, Transform transform)
+    {
         // move the screen to the panel's position
-        clientScreen1.transform.position = transform.position;
-        clientScreen1.transform.rotation = transform.rotation * Quaternion.Euler(0, 0, 0);
+        screen.transform.position = transform.position;
+        screen.transform.rotation = transform.rotation;
+    }
 
-        if (Input.GetKeyUp(mfdcampos.x < 0 ? KeyCode.F1 : KeyCode.F2))
-        {
-            clientCanvas.enabled = !clientCanvas.enabled;
-        }
+    /// <summary>
+    /// With the screen resolution change, it can set the screen position at the proper positions.
+    /// MFD1 goes to left bottom and MFD2 goes to right bottom.
+    /// </summary>
+    /// <param name="c"></param>
+    /// <param name="side"></param>
+    static void UpdateCanvasPositionOnScreen(Canvas c, float side)
+    {
+        // raw image
+        var rawImage = c.transform.GetChild(0).GetComponent<RectTransform>();
+        // canvas rect
+        var canvasRect = c.GetComponent<RectTransform>(); 
 
-        UpdateCanvas(clientCanvas, mfdcampos.x);
+        // if the screen height below 720p then use the screen height, otherwise use 2/3 of the screen height
+        // this makes the MFD scren readable for any resolution.
 
+        var size = Screen.height * (2.0f / 3.0f) < 720 ? Screen.height : Screen.height * (2.0f / 3.0f);
+
+        rawImage.sizeDelta = new Vector2(size * 1.0f, size);
+        rawImage.transform.position = new Vector3(
+            canvasRect.position.x + canvasRect.sizeDelta.x * 0.5f * side + rawImage.sizeDelta.x * 0.5f * -side,
+            canvasRect.position.y - canvasRect.sizeDelta.y * 0.5f + rawImage.sizeDelta.y * 0.5f, 0.0f);
+    }
+
+    static void RenderCPUExample(RenderTexture renderTexture)
+    {
         // render teture
-        return;
         Texture2D tex = new Texture2D(256, 256, TextureFormat.RGBA32, false);
 
         IEnumerable<Vector2> pix =
@@ -67,17 +115,6 @@ public class MFD : MonoBehaviour
         tex.Apply();
 
         Graphics.CopyTexture(tex, renderTexture);
-    }
-    void UpdateCanvas(Canvas c, float side)
-    {
-        var ri = c.transform.GetChild(0).GetComponent<RectTransform>(); // raw image
-        var cr = c.GetComponent<RectTransform>(); // canvas rect
-        var size = Screen.height * (2.0f / 3.0f) < 720 ? Screen.height : Screen.height * (2.0f / 3.0f);
-
-        ri.sizeDelta = new Vector2(size * 1.0f, size);
-        ri.transform.position = new Vector3(
-            cr.position.x + cr.sizeDelta.x * 0.5f * side + ri.sizeDelta.x * 0.5f * -side,
-            cr.position.y - cr.sizeDelta.y * 0.5f + ri.sizeDelta.y * 0.5f, 0.0f);
     }
 
     static void DrawLine(Texture2D a_Texture, int x1, int y1, int x2, int y2, int lineWidth, Color a_Color)
--- Assets/Main.cs
+++ Assets/Main.cs
@@ -8,27 +8,21 @@ using UnityEngine.UI;
 public class Main : MonoBehaviour {
 
     [SerializeField] public GameObject player;
-
     [SerializeField] public GameObject mfdpanel1;
     [SerializeField] public GameObject mfdpanel2;
-
     [SerializeField] public Camera mainCamera;
 
     static public GameObject clientPlayer;
-
     static public GameObject clientMfdPanel1;
     static public GameObject clientMfdPanel2;
-
     static public Camera clientMainCamera;
 
     // Use this for initialization
     void Start ()
     {
         clientPlayer = Instantiate(player);
-
         clientMfdPanel1 = Instantiate(mfdpanel1);
         clientMfdPanel2 = Instantiate(mfdpanel2);
-
         clientMainCamera = mainCamera;
     }
 	

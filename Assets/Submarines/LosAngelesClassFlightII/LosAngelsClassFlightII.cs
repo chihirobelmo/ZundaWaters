a6e95bb810c96289ef3bb888d4416ee5a09bf8f5
chihirobelmo <chihirobelmo@gmail.com>
2024-04-29 23:15:59 +0900

    PID thrust control and some refactor

--- Assets/Submarines/LosAngelesClassFlightII/LosAngelsClassFlightII.cs
+++ Assets/Submarines/LosAngelesClassFlightII/LosAngelsClassFlightII.cs
@@ -21,9 +21,19 @@ public class LosAngelsClassFlightII : MonoBehaviour
     const float kLengthMeter = 110.0f;
     const float kRadiusMeter = 5.0f;
     const float kWaterlineMeter = +1.0f;
+    public Bell currentBell = Bell.DeadSlowAhead;
+    public float targetThrustN = TargetThrustN(Bell.DeadSlowAhead);
+    public float targetPitchDeg = 0;
+    public float targetRudderDeg = 0;
 
-    enum Bell
+    PID thrustController = new PID(0.1f, 1.0f, 0);
+    PID pitchController = new PID(0.1f, 5.0f, 0);
+
+    
+
+    public enum Bell : int
     {
+        FlankAhead,
         FullAhead,
         HalfAhead,
         SlowAhead,
@@ -32,40 +42,11 @@ public class LosAngelsClassFlightII : MonoBehaviour
         DeadSlowAstern,
         SlowAstern,
         HalfAstern,
-        FullAstern
+        FullAstern,
+        FlankAstern
     }
 
     public bool IsPropellerUnderWater => Object3DPropellerAxis.position.y <= 0;
-
-    /// <summary>
-    /// Ship Forward Vector in world space.
-    /// </summary>
-    public Vector3 Forward => transform.forward;
-
-    /// <summary>
-    /// Ship Right Vector in world space.
-    /// </summary>
-    public Vector3 Right => transform.right;
-
-    /// <summary>
-    /// Ship Up Vector in world space.
-    /// </summary>
-    public Vector3 Up => transform.up;
-
-    /// <summary>
-    /// Ship Position in world space.
-    /// </summary>
-    public Vector3 Pos { get => transform.position; set => transform.position = value; }
-
-    /// <summary>
-    /// Ship Rotation in quaternion.
-    /// </summary>
-    public Quaternion Rot { get => transform.rotation; set => transform.rotation = value; }
-
-    /// <summary>
-    /// Rotation Speed in degree per second the ship rotates to fwd/aft (x-axis).
-    /// </summary>
-    public float FwdRotationSpeedDeg { get => angularSpeedDeg.x; set => angularSpeedDeg.x = value; }
 
     /// <summary>
     /// Angle aileron in degree per second.
@@ -97,54 +78,43 @@ public class LosAngelsClassFlightII : MonoBehaviour
     /// </summary>
     [SerializeField] Vector3 angularSpeedDeg;
 
-    private Vector3 lastPosition_;
-
-    public Vector3 LastPosition { get => lastPosition_; set => lastPosition_ = Pos; }
+    private Vector3 lastPosition;
 
     /// <summary>
     /// Ship Movement Vector
     /// </summary>
-    public Vector3 Vector => Pos - LastPosition;
+    public Vector3 Vector => transform.position - lastPosition;
     public float Knots => Vector.magnitude * KTS_TO_MPS;
 
     // Use this for initialization
     void Start()
     {
-        velocityMPS = Forward * 5.0f/*knots*/ * KTS_TO_MPS;
-        LastPosition = Pos;
-    }
-
-    LosAngelsClassFlightII updateLastPosition()
-    {
-        LastPosition = Pos;
-        return this;
+        velocityMPS = transform.forward * 5.0f/*knots*/ * KTS_TO_MPS;
+        lastPosition = transform.position;
     }
 
     LosAngelsClassFlightII UpdateVelocity()
     {
         // propeller under the  water
         if (IsPropellerUnderWater)
-        {
-            velocityMPS += VtDt(waterDrag, kMass, Forward * thrustN, velocityMPS) * dt;
-        }
-        // cancel inartial velocity
-        velocityMPS -= Vector3.ProjectOnPlane(velocityMPS, Right) * dt;
-        velocityMPS -= Vector3.ProjectOnPlane(velocityMPS, Up) * dt;
+            velocityMPS += VtDt(waterDrag, kMass, transform.forward * thrustN, velocityMPS) * dt;
+        // cancel inartial velocity to ship right and up axis
+        velocityMPS -= Vector3.ProjectOnPlane(velocityMPS, transform.right) * dt;
+        velocityMPS -= Vector3.ProjectOnPlane(velocityMPS, transform.up) * dt;
         // TBD: calc drag to cancel inartial velocity, use ElipsoidProjectedAreaM2.
         return this;
     }
 
     LosAngelsClassFlightII UpdatePosition() {
-        Pos += velocityMPS * dt;
+        transform.position += velocityMPS * dt;
         return this;
     }
     LosAngelsClassFlightII CalcAndApplyGravityAndFloat() {
 
-
         // Divide ship to each cell
         IEnumerable<Vector3> eachCellOfShip =
             from z in new int[] { -55, -27, 27, 55 }
-            select Pos + Forward * z;
+            select transform.position + transform.forward * z;
 
         int cellCount = eachCellOfShip.Count();
 
@@ -153,17 +123,23 @@ public class LosAngelsClassFlightII : MonoBehaviour
         {
             // calculate N from gravity and float at each cell.
             // remember: v = omega * radius
-            float omega = FwdRotationSpeedDeg;
-            float radius = /*avoid 0 divide*/0.001f + (/*offset*/p.z - /*gravity center*/Pos.z);
+            float omega = angularSpeedDeg.x;
+            float radius = /*avoid 0 divide*/0.001f + (/*offset*/p.z - /*gravity center*/transform.position.z);
 
-            Vector3 dv = p.y > 0 ?
-            VtDt(waterDrag, kMass, -Vector3.up * gravity, angularSpeedDeg * Mathf.Deg2Rad * radius) * dt * (1.0f / cellCount) : // in air
-            VtDt(waterDrag, kMass, -Vector3.up * (gravity - ballastAirMPS2), angularSpeedDeg * Mathf.Deg2Rad * radius) * dt * (1.0f / cellCount); // under water
-            dv.x = 0; dv.z = 0; // TBD: not sure why but only calculating y axis were not working well...so we remove this from Vector3.
+            // calc dV = dOmega * radius
+            Vector3 dV = p.y > 0 ?
+            // in air, current speed is considered "angular speed * radius", so angular speed is the omega
+            VtDt(waterDrag, kMass, -Vector3.up * gravity, angularSpeedDeg * Mathf.Deg2Rad * radius) * dt * (1.0f / cellCount) :
+            // under water
+            VtDt(waterDrag, kMass, -Vector3.up * (gravity - ballastAirMPS2), angularSpeedDeg * Mathf.Deg2Rad * radius) * dt * (1.0f / cellCount);
+            // TBD: not sure why but only calculating y axis were not working well...
+            // and calc Vector3 like we do now makes X axis movement...so we remove this from Vector3.
+            dV.x = 0; dV.z = 0;
 
-            // apply N to velocity;
-            velocityMPS += dv;
-            FwdRotationSpeedDeg -= Mathf.Rad2Deg * dv.y * radius;
+            // apply dV to velocity;
+            velocityMPS += dV;
+            // apply dV = omega * radius to angular speed, consider dV = Omega. cos more radius more speed should apply...
+            angularSpeedDeg.x -= Mathf.Rad2Deg * dV.y * radius;
         });
         return this;
     }
@@ -173,10 +149,11 @@ public class LosAngelsClassFlightII : MonoBehaviour
         const float maxYawDegPerSecond = 360.0f;
 
         // yaw and pitch
+        // somehow input yaw also inputs pitch so placeholder for now.
         angularSpeedDeg.y = angleRudderDeg; // Mathf.Clamp(angularSpeedDeg.y.Deg() + (angleRudderDeg - angularSpeedDeg.y).Degt() * dt, -80, 80);
         angularSpeedDeg.x = angleAileronDeg; // Mathf.Clamp(angularSpeedDeg.x.Deg() + (angleAileronDeg - angularSpeedDeg.x).Degt() * dt, -40, 40);
 
-        Rot *= Quaternion.Euler((angularSpeedDeg * Mathf.Deg2Rad).Radt() * dt);
+        transform.rotation *= Quaternion.Euler((angularSpeedDeg * Mathf.Deg2Rad).Radt() * dt);
 
         return this;
     }
@@ -194,7 +171,7 @@ public class LosAngelsClassFlightII : MonoBehaviour
     /// force roll to 0.
     /// </summary>
     LosAngelsClassFlightII StabilizeRoll() {
-        Rot = Quaternion.Euler(transform.eulerAngles.x.Deg(), transform.eulerAngles.y.Deg(), 0.0f.Deg());
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x.Deg(), transform.eulerAngles.y.Deg(), 0.0f.Deg());
         return this;
     }
 
@@ -207,10 +184,34 @@ public class LosAngelsClassFlightII : MonoBehaviour
         const float leftRotation = -1.0f;
         const float propellerRotationRatio = /*make it 1 rad at certain thrust*/(1.0f / /*thrust value where rotation becomes 1.0 rad*/10.0f);
         new List<Transform> { Object3DPropellerBlades, Object3DPropellerAxis }
-        .ForEach(x => x.Rotate(Forward, leftRotation * (thrustN / propellerRadiusMeter) * propellerRotationRatio, Space.World));
+        .ForEach(x => x.Rotate(transform.forward, leftRotation * (thrustN / propellerRadiusMeter) * propellerRotationRatio, Space.World));
 
         return this;
     }
+
+    static public float TargetThrustN(Bell bell)
+    {
+        switch (bell)
+        {
+            case Bell.FlankAhead: return 40.0f;
+            case Bell.FullAhead: return 30.0f;
+            case Bell.HalfAhead: return 20.0f;
+            case Bell.SlowAhead: return 10.0f;
+            case Bell.DeadSlowAhead: return 5.0f;
+            case Bell.Stop: return 0.0f;
+            case Bell.DeadSlowAstern: return -5.0f;
+            case Bell.SlowAstern: return -10.0f;
+            case Bell.HalfAstern: return -20.0f;
+            case Bell.FullAstern: return -30.0f;
+            case Bell.FlankAstern: return -40.0f;
+            default: return 0.0f;
+        }
+    }
+
+    static public float TargetValueVector(float targetvalue, float currentValue, float startSpeedDecentRange)
+        => (targetvalue - currentValue > 0 ? 1.0f : -1.0f) * Mathf.Clamp(Mathf.Abs(targetvalue - currentValue) / startSpeedDecentRange, 0.0f, 1.0f);
+
+    float lastTimePressed;
 
     LosAngelsClassFlightII UserControl()
     {
@@ -221,19 +222,34 @@ public class LosAngelsClassFlightII : MonoBehaviour
         const float leftYaw = -1.0f;
         const float rightYaw = +1.0f;
 
+        if (Input.anyKeyDown)
+            lastTimePressed = Time.time;
+
+        // KeyUP
         new Dictionary<KeyCode, System.Action> {
             // Q: Thrust Uo
-            { KeyCode.Q, () => ChangeAndLimitThrust(thrustUp * kThrustChangeRate * dt) },
+            { KeyCode.Q, () => { targetThrustN = Mathf.Clamp(targetThrustN + 5.0f, -40.0f, 80.0f); } },
             // Z: Thrust Down
-            { KeyCode.Z, () => ChangeAndLimitThrust(thrustDown * kThrustChangeRate * dt) },
-            // A: Yaw Left
-            { KeyCode.A, () => ChangeAndLimitYawAngularSpeed(leftYaw * kSurfaceChangeRate * dt) },
-            // D: Yaw Right
-            { KeyCode.D, () => ChangeAndLimitYawAngularSpeed(rightYaw * kSurfaceChangeRate * dt) },
+            { KeyCode.Z, () => { targetThrustN = Mathf.Clamp(targetThrustN - 5.0f, -40.0f, 80.0f); } },
+            // W: Pitch Down
+            { KeyCode.W, () => { targetPitchDeg = Mathf.Clamp(targetPitchDeg + 5.0f, -30.0f, 30.0f); } },
+            // S: Pitch Up
+            { KeyCode.S, () => { targetPitchDeg = Mathf.Clamp(targetPitchDeg - 5.0f, -30.0f, 30.0f); } },
+        }
+        .ToList()
+        .Select(x => { if (Input.GetKeyUp(x.Key)) x.Value(); return 0; })
+        .Sum();
+
+        // KeyHold
+        new Dictionary<KeyCode, System.Action> {
             // W: Pitch Down
             { KeyCode.W, () => ChangeAndLimitPitchAngularSpeed(pitchDown * kSurfaceChangeRate * dt) },
             // S: Pitch Up
             { KeyCode.S, () => ChangeAndLimitPitchAngularSpeed(pitchUp * kSurfaceChangeRate * dt) },
+            // A: Yaw Left
+            { KeyCode.A, () => ChangeAndLimitYawAngularSpeed(leftYaw * kSurfaceChangeRate * dt) },
+            // D: Yaw Right
+            { KeyCode.D, () => ChangeAndLimitYawAngularSpeed(rightYaw * kSurfaceChangeRate * dt) },
             // E: Ballast more air
             { KeyCode.E, () => { ballastAirMPS2 = Mathf.Clamp(ballastAirMPS2 + kBallastChangeRate * dt, kMinBallast, kMaxBallast); } },
             // C: Ballast more water
@@ -252,16 +268,29 @@ public class LosAngelsClassFlightII : MonoBehaviour
         return this;
     }
 
+    static float PIDAndLimitThrustN(PID pid, float thrustN, float targetThrustN)
+    {
+        thrustN += Mathf.Clamp(pid.run(thrustN, targetThrustN), -10.0f, 10.0f) * dt;
+        return Mathf.Clamp(thrustN, -10.0f, 40.0f);
+    }
+
     // Update is called once per frame
-    void Update() =>
-        UserControl()
-        .UpdateVelocity()
-        .CalcAndApplyGravityAndFloat()
-        .UpdatePosition()
-        .UpdateOrientaion()
-        .StabilizeRoll()
-        .Animation()
-        .updateLastPosition();
+    void Update()
+    {
+        if (Main.offsetForReset.magnitude > 0) { return; }
+
+        UserControl();
+
+        thrustN = PIDAndLimitThrustN(thrustController, thrustN, targetThrustN);
+
+        UpdateVelocity();
+        UpdatePosition();
+        UpdateOrientaion();
+        CalcAndApplyGravityAndFloat();
+        StabilizeRoll();
+        Animation();
+        EndFrameJob();
+    }
 
     private void EndFrameJob()
     {
@@ -273,14 +302,36 @@ public class LosAngelsClassFlightII : MonoBehaviour
         lastPosition = transform.position;
 
         // floating point origin reset
-        if (Mathf.Abs(transform.position.x) >= 256)
+        if (transform.position.x >= 256)
         {
-            lastPosition = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            lastPosition = new Vector3(
+                transform.position.x - 256f * 2.0f,
+                transform.position.y,
+                transform.position.z);
             Main.offsetForReset = lastPosition - transform.position;
         }
-        if (Mathf.Abs(transform.position.z) >= 256)
+        if (transform.position.z >= 256)
         {
-            lastPosition = new Vector3(transform.position.x, transform.position.y, -transform.position.z);
+            lastPosition = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z - 256f * 2.0f);
+            Main.offsetForReset = lastPosition - transform.position;
+        }
+        if (transform.position.x <= -256)
+        {
+            lastPosition = new Vector3(
+                transform.position.x + 256f * 2.0f,
+                transform.position.y,
+                transform.position.z);
+            Main.offsetForReset = lastPosition - transform.position;
+        }
+        if (transform.position.z <= -256)
+        {
+            lastPosition = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z + 256f * 2.0f);
             Main.offsetForReset = lastPosition - transform.position;
         }
     }
---
+++ Assets/Submarines/LosAngelesClassFlightII/PID.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static StaticMath;
+
+/// <summary>
+/// from https://controlabo.com/pid-program/
+/// </summary>
+public class PID
+{
+    // 定数設定 ===============
+    float KP = 0.0001f;  // Pゲイン
+    float KD = 0;   // Dゲイン
+    float KI = 0;   // Iゲイン
+
+    // 変数初期化 ===============
+    float e_pre = 0; // 微分の近似計算のための初期値
+    float ie = 0;    // 積分の近似計算のための初期値
+
+    public PID() { }
+
+    public PID(float kp, float kd, float ki) {
+        KP = kp;
+        KD = kd;
+        KI = ki;
+    }
+
+    public float run(float current, float target) {
+
+        // 現時刻における情報を取得
+        float y = current; // 出力を取得。例:センサー情報を読み取る処理
+        float r = target; // 目標値を取得。目標値が一定ならその値を代入する
+
+        // PID制御の式より、制御入力uを計算
+        float e = r - y;                // 誤差を計算
+        float de = (e - e_pre) / dt;        // 誤差の微分を近似計算
+        ie = ie + (e + e_pre) * dt / 2; // 誤差の積分を近似計算
+        float u = KP * e + KI * ie + KD * de; // PID制御の式にそれぞれを代入
+
+        // 次のために現時刻の情報を記録
+        e_pre = e;
+
+        return u;
+    }
+}
+
+/* https://qiita.com/RyoH_/items/373f6451c4946b1e447e
+パラメータチューニング
+PID制御では
+,
+,
+の値を設定する必要があります．様々な方法がありますが，試行錯誤で適切な値を探ることにします．
+
+ステップ１
+,
+KI,KDの値を0とし，
+KPのみで制御する．
+KPの値が小さければ，自動車の振動は小さいものの，目標に達するまで時間がかかります（本シミュレーションの場合はハンドルを切るのが遅くてコースアウトしてしまいます）．逆に
+KPの値を大きくしすぎると，自動車が目標値を超えて大きく振動してしまいます．小さすぎず，大きすぎない値を見つける必要があります．
+
+ステップ２
+KDに値を入れてみる．次に微分ゲインに数値を入れてみます．
+KDが小さいとほぼ先ほどと大きくは変わりません．大きくしすぎると，ハンドル角の変化が小さくなりすぎてしまい，制御することできなくなります．適切な値を選ぶと，オーバーシュートが小さくなり，ほぼ目標値に追従して進むようになります．
+*/
